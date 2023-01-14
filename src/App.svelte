50cd5c3788025787de2a38312f722ffc62be8dfc
unknown <farisahmad1210@gmail.com>
2023-01-15 00:47:44 +0700

    Firts Commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<div class="header">
+  <div id="head">
+    <input class="search" type="text" placeholder="Search ">
+    <a href=""><img class="img" src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png" alt=""></a>
+
+  </div>
+</div>
